72d53cfd91fa77ea95c0b5223dd0d2560c6669d3
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-04-28 21:33:50 +0200

    Ticket#2019040967001459 — OSS4 - Radius / Proxy ￼Update must not rewrite config.

--- tools/update.sh
+++ tools/update.sh
@@ -37,23 +37,18 @@ fi
 cd /usr/share/oss/templates/radius/
 for i in $( find -type f )
 do
-	cp $i /etc/raddb/$i
+	if [ "$i" = "clients.conf" ]; then
+		continue
+	else
+		cp /etc/raddb/$i /etc/raddb/$i.$DATE
+		cp $i /etc/raddb/$i
+        fi	       
 done
 ln -fs  ../mods-available/set_logged_on /etc/raddb/mods-enabled/set_logged_on
 
 sed -i "s#SCHOOL_SERVER_NET#${SCHOOL_SERVER_NET}#" /etc/raddb/clients.conf
 sed -i "s#SCHOOL_WORKGROUP#${SCHOOL_WORKGROUP}#"   /etc/raddb/mods-available/mschap
-#Setup customized certificates
-for i in ca.cnf  client.cnf  server.cnf
-do
-	sed -i "s/#NAME#/${SCHOOL_NAME}/"     /etc/raddb/certs/$i
-	sed -i "s/#DOMAIN#/${SCHOOL_DOMAIN}/" /etc/raddb/certs/$i
-done
 
-cd /etc/raddb/certs/
-rm -f *.pem *.der *.csr *.crt *.key *.p12 serial* index.txt*
-./bootstrap
-
-systemctl enable radiusd
-systemctl start  radiusd
+systemctl enable  radiusd
+systemctl restart radiusd
 
