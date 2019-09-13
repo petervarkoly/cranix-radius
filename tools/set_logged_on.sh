ac77d4a2d3195bc3c0ca114e0524e89898bbbb89
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-09-13 20:50:16 +0200

    Autoregister new devices via radius exec script.

--- tools/set_logged_on.sh
+++ tools/set_logged_on.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+# This script sends the MAC address and the user name to the oss-api.
+# If the client was already registered the LoggedIn of the user will be set.
+# If the client was not already registered the device will be registered.
+# If the registration was successfully (OK) wait a litle bit an break the connection
+# to trigger a reconnection to get the new ip address.
+# If the registration was not successfully the client stands in the ANON_DCHP range.
+# Copiright Dipl.-Ing. Peter Varkoly <peter@varkoly.de>
 
 if [ "$2" ]
 then
-        curl -s -X PUT "http://localhost:9080/api/devices/loggedInUserByMac/$2/$1" &> /dev/null
+	OUT=$( curl -s -X PUT "http://localhost:9080/api/selfmanagement/addDeviceToUser/$2/$1"  )
+	case "$OUT" in
+		ALREADY-REGISTERED)
+		        curl -s -X PUT "http://localhost:9080/api/devices/loggedInUserByMac/$2/$1" &> /dev/null
+			;;
+		OK)
+			sleep 10
+			exit 1
+			;;
+	esac
 fi
 exit 0
 
