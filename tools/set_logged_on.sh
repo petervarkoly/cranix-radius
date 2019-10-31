e6ad0142d122496b6f7f93bc7235165cf76bce4b
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2019-10-31 06:49:14 +0100

    Ticket#2019103067001899 — OSS4.1 FHBK - Radiusfehler

--- tools/set_logged_on.sh
+++ tools/set_logged_on.sh
@@ -2,11 +2,10 @@
 # This script sends the MAC address and the user name to the oss-api.
 # If the client was already registered the LoggedIn of the user will be set.
 # If the client was not already registered the device will be registered.
-# If the registration was successfully (OK) wait a litle bit an break the connection
-# to trigger a reconnection to get the new ip address.
-# If the registration was not successfully the client stands in the ANON_DCHP range.
+# If the registration was successfully (OK) wait a litle bit that the DHCP can be restarted to get the new ip address.
+# If the registration was not successfully the client stands in the ANON_DCHP range or braek the connection if SCHOOL_FORCE_REGISTER_DEVICE set yes
 # Copyright Dipl.-Ing. Peter Varkoly <pvarkoly@cephalix.eu>
-
+. /etc/sysconfig/schoolserver
 if [ "$2" ]
 then
 	MAC=$( echo $2 |  tr "-" ":" )
@@ -20,7 +19,12 @@ then
 			sleep 10
 			;;
 		*)
-			exit 1
+			if [ "${SCHOOL_FORCE_REGISTER_DEVICE}" = "yes" ]; then
+				echo "Device can not be registered."
+                                exit 1
+                        else
+                                curl -s -X PUT "http://localhost:9080/api/devices/loggedInUserByMac/$MAC/$1" &> /dev/null
+                        fi
 	esac
 fi
 exit 0
